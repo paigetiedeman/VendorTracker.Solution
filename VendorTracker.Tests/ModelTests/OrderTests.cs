79692274850b50065c9bd225cc163679f6ba5537
Paige Tiedeman <paigetiedeman@gmail.com>
2021-10-01 11:03:14 -0700

    test for Description added and passed

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -11,16 +11,25 @@ namespace VendorTracker.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newVendor = new Order("title test");
+      Order newVendor = new Order("title test", "description test");
       Assert.AreEqual(typeof(Order), newVendor.GetType());
     }
     [TestMethod]
     public void GetSetTitle_ReturnsOrderTitle_String()
     {
       string title = "Order 1";
-      Order newOrder = new Order(title);
+      Order newOrder = new Order(title, "description test");
       string result = newOrder.Title;
       Assert.AreEqual(title, result);
+    }
+    [TestMethod]
+    public void GetSetDescription_ReturnsOrderDescription_String()
+    {
+      string title = "Order 1";
+      string description = "1 Pastry";
+      Order newOrder = new Order(title, description);
+      string newDescription = newOrder.Description;
+      Assert.AreEqual(description, newDescription);
     }
   }
 }
--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -5,11 +5,13 @@ namespace VendorTracker.Models
   public class Order
   {
     public string Title { get; set; }
+    public string Description { get; set; }
     // Title, description, price, date
 
-    public Order(string title)
+    public Order(string title, string description)
     {
       Title = title;
+      Description = description;
     }
   }
 }
