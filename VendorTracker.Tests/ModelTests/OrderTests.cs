12b74b9f384f30a9de0f17085ac1053e9d338158
Paige Tiedeman <paigetiedeman@gmail.com>
2021-10-01 10:43:50 -0700

    test for Title added and passed

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -11,8 +11,16 @@ namespace VendorTracker.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newVendor = new Order();
+      Order newVendor = new Order("title test");
       Assert.AreEqual(typeof(Order), newVendor.GetType());
+    }
+    [TestMethod]
+    public void GetSetTitle_ReturnsOrderTitle_String()
+    {
+      string title = "Order 1";
+      Order newOrder = new Order(title);
+      string result = newOrder.Title;
+      Assert.AreEqual(title, result);
     }
   }
 }
--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -4,6 +4,12 @@ namespace VendorTracker.Models
 {
   public class Order
   {
-    
+    public string Title { get; set; }
+    // Title, description, price, date
+
+    public Order(string title)
+    {
+      Title = title;
+    }
   }
 }
