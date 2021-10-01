bddfe331a0fd52733d43d9c52f0559ec08c659df
Paige Tiedeman <paigetiedeman@gmail.com>
2021-10-01 12:16:18 -0700

    WIP: Controllers and Vendor List

--- VendorTracker/Controllers/VendorController.cs
+++ VendorTracker/Controllers/VendorController.cs
@@ -6,6 +6,12 @@ namespace VendorTracker.Controllers
 {
   public class VendorController : Controller
   {
-
+    [HttpGet("/vendor")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetVendors();
+      return View(allVendors);
+    }
+    
   }
 }
